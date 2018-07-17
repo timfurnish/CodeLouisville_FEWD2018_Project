46d732881dcffa75eef3f806636df7ae4f892fc8
timfurnish <38260772+timfurnish@users.noreply.github.com>
2018-07-17 14:51:56 -0400

    add comment to toc.js

--- js/toc.js
+++ js/toc.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     console.log(data);
     var statusHTML = '';
     var currentYear = '';
+    /* Loop through and build an unordered list from the array in the articles.json file, and whenever the article year changes, add an h5 to separate the lists. */
     $.each(data,function (index, article) {
         if (currentYear != article.year) {
           if (index !=0) {statusHTML += '</ul>'}
